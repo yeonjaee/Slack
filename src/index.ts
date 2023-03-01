ce09fce61699feb9c3a2c20ea69f123aea12ccee
steve <siriyay@naver.com>
2023-03-01 17:54:20 +0900

    env

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
+require('dotenv').config()
 import express from 'express';
 import { WebClient } from '@slack/web-api';
 import {createEventAdapter, ErrorCode} from '@slack/events-api';
 import { createServer } from 'http';
 
-// 생성한 슬랙봇에 대한 키값들
-import CONFIG from '../config/bot.json';
-
 // 슬랙에서 슬랙봇에게 접근가능한 엔드포인트를 만들기 위해 웹서버(express)를 사용
 const app = express();
 
-const slackEvents = createEventAdapter(CONFIG.SIGNING_SECRET);
-const webClient = new WebClient(CONFIG.BOT_USER_OAUTH_ACCESS_TOKEN)
+// env 변수
+const signingSecret = process.env.SIGNING_SECRET;
+const botToken = process.env.BOT_USER_OAUTH_ACCESS_TOKEN ;
+
+const slackEvents = createEventAdapter(signingSecret);
+const webClient = new WebClient(botToken)
 
 // 메시지 이벤트 구독하기
 slackEvents.on('message', async (event) => {
-    console.log(event);
+    console.log(event.text);
 try{
-    if (event.text === '스탠드업') {
+    if (event.text === 'test') {
         await webClient.chat.postMessage({
             user:'',
-            text: `안녕하세요 <@유저아이디>!\n할 일을 보여줍니다.. 이건 테스트입니다.`,
+            text: `안녕하세요 <@${event.user}>!\n이건 테스트입니다.`,
             channel: event.channel,
-
         });
     }
 }catch (e) {
@@ -38,8 +39,6 @@ app.use('/slack/events', slackEvents.requestListener());
 
 // express 웹 서버 실행
 // npx ts-node ./src/index.ts
-// ngrok 으로 생성한 외부 포트를 슬랙의 Request URL 에 등록.
-// https://~.ngrok.io/slack/events
 createServer(app).listen(8080, () => {
     console.log('slack bot 시작');
 });
